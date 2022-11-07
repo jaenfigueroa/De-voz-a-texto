4fe88d4a2959ee406f89faa87b4b5e16361c1622
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-11-07 13:57:38 -0500

    agrege el estado de la aplicacion

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
+const microfono = document.getElementById('microfono')
 const contenedorResultado = document.querySelector('#resultado')
-const boton = document.querySelector('#boton')
+const botonEscuchar = document.getElementById('boton-escuchar')
+const botonApagar = document.getElementById('boton-apagar')
 
-boton.addEventListener('click', escuchar)
+botonEscuchar.addEventListener('click', escuchar)
+botonApagar.addEventListener('click', apagar)
+
+///////////////////////////////////////////////////////
+const foxed = new webkitSpeechRecognition();
+
+foxed.lang = "es-ES";
+foxed.continuous = true;
+
+foxed.onresult = (evento) => {
+  for (const result of evento.results) {
+    contenedorResultado.innerHTML = result[0].transcript;
+  }
+};
 
 ///////////////////////////////////////////////////////
 function escuchar() {
-  const foxed = new webkitSpeechRecognition();
+  console.log('se empezo a escuchar');
 
-  foxed.lang = "es-ES";
-  foxed.continuous = true;
+  botonEscuchar.style.display = 'none'
+  botonApagar.style.display = 'inline'
 
-  foxed.onresult = (evento) => {
-    for (const result of evento.results) {
-      contenedorResultado.innerHTML = result[0].transcript;
-    }
-  };
+  microfono.classList.add('main__estado--activo')
 
   foxed.start();
 }
 
+function apagar() {
+  console.log('se dejo de escuchar');
+
+  botonEscuchar.style.display = 'inline'
+  botonApagar.style.display = 'none'
+
+  microfono.classList.remove('main__estado--activo')
+
+  foxed.stop();
+}
